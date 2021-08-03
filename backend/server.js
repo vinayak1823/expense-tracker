7d39017b25fbfce3b049e3c8c9676a72535dcdfd
vinayak1823 <vinayak18dec@gmail.com>
2021-08-03 19:59:13 +0530

    Made app ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const path = require('path')
 
 dotenv.config()
 
@@ -20,5 +21,16 @@ if (process.env.NODE_ENV === 'development') {
 const port = process.env.PORT
 
 app.use('/api/transactions', transactions)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
 
 app.listen(port, () => console.log(`Port running on ${port}`))
